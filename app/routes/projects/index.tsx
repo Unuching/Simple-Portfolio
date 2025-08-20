ca4b84363f31aa3d853a238165e952e6051a73a7
Unuching Marma <unuching.marma2023@gmail.com>
2025-08-20 20:45:25 +0600

    Auto: 1 modified - 2025-08-20

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,6 +1,7 @@
 import type { Route } from './+types/index';
 import type { Project } from '~/types';
 import ProjectCard from '~/components/peojectCard';
+import { useState } from 'react';
 
 export async function loader({
   request,
@@ -14,6 +15,32 @@ export async function loader({
 const ProjectPage = ({ loaderData }: Route.ComponentProps) => {
   const { projects } = loaderData as { projects: Project[] };
   console.log(projects);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const projectPerPage = 4;
+  const totalPage = Math.ceil(projects.length / projectPerPage);
+
+  const indexOfLast = currentPage * projectPerPage;
+  const indexOfFirst = indexOfLast - projectPerPage;
+  const currentProjects = projects.slice(indexOfFirst, indexOfLast);
+
+  const renderPagination = () => (
+    <div className='flex justify-center gap-2 mt-8'>
+      {Array.from({ length: totalPage }, (_, idx) => (
+        <button
+          key={idx + 1}
+          className={`px-3 py-1 cursor-pointer rounded ${
+            currentPage === idx + 1
+              ? 'bg-blue-600 text-white'
+              : 'bg-gray-700 text-gray-200'
+          }`}
+          onClick={() => setCurrentPage(idx + 1)}
+        >
+          {idx + 1}
+        </button>
+      ))}
+    </div>
+  );
 
   return (
     <>
@@ -21,10 +48,11 @@ const ProjectPage = ({ loaderData }: Route.ComponentProps) => {
         My projects
       </h2>
       <div className='grid gap-6 md:grid-cols-2'>
-        {projects.map((project) => (
+        {currentProjects.map((project) => (
           <ProjectCard project={project} key={project.id} />
         ))}
       </div>
+      {totalPage > 1 && renderPagination()}
     </>
   );
 };

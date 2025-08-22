2b9bb6ba001638084103767af2c87149a4f1eb19
Unuching Marma <unuching.marma2023@gmail.com>
2025-08-22 08:19:49 +0600

    Auto: 1 modified - 2025-08-22

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -19,6 +19,52 @@ const ContactPage = () => {
             className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-800'
           />
         </div>
+        <div>
+          <label
+            htmlFor='email'
+            className='block text-lg font-medium text-gray-300'
+          >
+            Email
+          </label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-800'
+          />
+        </div>
+        <div>
+          <label
+            htmlFor='subject'
+            className='block text-lg font-medium text-gray-300'
+          >
+            Subject
+          </label>
+          <input
+            type='text'
+            id='subject'
+            name='subject'
+            className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-800'
+          />
+        </div>
+        <div>
+          <label
+            htmlFor='message'
+            className='block text-lg font-medium text-gray-300'
+          >
+            Message
+          </label>
+          <textarea
+            name='message'
+            id='Message'
+            className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-800'
+          >
+            Type Your Message Here...
+          </textarea>
+        </div>
+        <button className='w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-800 cursor-pointer'>
+          Send Your Message
+        </button>
       </form>
     </div>
   );

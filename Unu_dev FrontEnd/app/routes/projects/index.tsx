ad4b071c7f0faa94c18d978bf54573f305285aa6
Unuching Marma <unuching.marma2023@gmail.com>
2025-08-27 20:34:13 +0600

    Auto: 1 added, 2 modified - 2025-08-27

--- Unu_dev FrontEnd/app/routes/projects/index.tsx
+++ Unu_dev FrontEnd/app/routes/projects/index.tsx
@@ -4,13 +4,26 @@ import ProjectCard from '~/components/peojectCard';
 import { useState } from 'react';
 import Pagination from '~/components/pagination';
 import { AnimatePresence, motion } from 'framer-motion';
+import { it } from 'node:test';
 
 export async function loader({
   request,
 }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
   // const res = await fetch(`http://localhost:8000/projects`);
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
-  const data = await res.json();
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/projects?populate=*`
+  );
+  const json = await res.json();
+
+  const projects = json.data.map((item)=>({
+    id:item.id,
+    documentId: item.documentId,
+    title: item.title,
+    description:item.description,
+    image:
+
+
+  }))
 
   return { projects: data };
 }

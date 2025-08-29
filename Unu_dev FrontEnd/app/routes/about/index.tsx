aeb852778d0959f0dc199b909378cbdaf7a753d8
Unuching Marma <unuching.marma2023@gmail.com>
2025-08-29 21:34:08 +0600

    Auto: 1 modified - 2025-08-29

--- Unu_dev FrontEnd/app/routes/about/index.tsx
+++ Unu_dev FrontEnd/app/routes/about/index.tsx
@@ -9,7 +9,7 @@ const AboutPage = () => {
         />
         <div>
           <h1 className='text-3xl font-bold text-white mb-2'>
-            Hey, I'am Unu. I am a developer.
+            Hey, I'am Unu. I am a Web developer.
           </h1>
           <p className='text-gray-300 text-lg'>
             Hello, I'm Unuching Marma from Bangladesh, a passionate web
